fe6aa61278829339cbd5ff87cd0d9f0092b1382f
JanNabil73 <168550877+JanNabil73@users.noreply.github.com>
2024-05-01 00:28:22 +0300

    Update uart.c
    initialization of SYSTICKTIMER and a function that waits for 1 ms and the other waits for delay ms.

--- uart.c
+++ uart.c
@@ -74,5 +74,24 @@ uint8_t uart2_read_byte(void)
 	
 	return c; 
 }
+void SYSTICKTIMER_init(void){
+	NVIC_ST_CTRL_R = 0;
+	NVIC_ST_RELOAD_R = 0XFFFFFF;
+	NVIC_ST_CURRENT_R = 0;
+	NVIC_ST_CTRL_R = 0X05 ;
+}
 
 
+void SYSTICK_wait1ms(void){
+	NVIC_ST_RELOAD_R = 16000;
+	NVIC_ST_CURRENT_R = 0;
+	while((NVIC_ST_CTRL_R&0x10000)==0){}; // wait for the flag
+}
+
+
+void delayMillis(uint32_t delay){
+	unsigned long i;
+	for(i = 0 ; i<delay ; i++){
+		SYSTICK_wait1ms();
+	}
+}
