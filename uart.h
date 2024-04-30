b445ac2491821763c12846bd3767a7952b899187
fkamalo <167641692+fkamalo@users.noreply.github.com>
2024-05-01 00:02:26 +0300

    uart.h

--- uart.h
+++ uart.h
@@ -1,1 +1,28 @@
+#ifndef _TM4C_H
+#define _TM4C_H
+#include "tm4c123gh6pm.h"
+#endif
+#ifndef _STDINT_H
+#define _STDINT_H
+#include <stdint.h>
+#endif
+#ifndef _UART_H
+#define _UART_H
+
+void uart2_init();
+
+void uart2_send_byte(uint8_t c);
+
+uint8_t uart2_read_byte(void);
+void Uart5_init(void);
+void uart5_send_byte(uint8_t c);
+void Uart5_output_string(char* pt);
+
+void SYSTICKTIMER_init(void);
+void SYSTICK_wait1ms(void);
+void delayMillis(uint32_t delay);
+
+
+#endif
+
 
