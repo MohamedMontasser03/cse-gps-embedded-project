5af29df0ad693aa3a804ccfcde9901d0eea92464
Mohamed Montasser <90785835+MohamedMontasser03@users.noreply.github.com>
2024-05-01 15:36:29 +0300

    added defenitions for led.c

--- led.h
+++ led.h
@@ -1,1 +1,26 @@
+#ifndef _TM4C_H
+#define _TM4C_H
+#include "tm4c123gh6pm.h"
+#endif
 
+#ifndef _STDIO_H
+#define _STDIO_H
+#include <stdio.h>
+#endif
+
+#ifndef _LED_H
+#define _LED_H
+
+#define RED_LED 0x02
+#define BLUE_LED 0x04
+#define GREEN_LED 0x08
+#define RGB_LED 0x0E 
+#define SW1       0x10 
+#define SW2       0x01 
+
+void RGB_set(char mask);
+void RGB_clear(char mask);
+void RGB(char colour);
+
+
+#endif
