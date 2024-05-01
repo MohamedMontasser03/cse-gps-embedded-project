79bb6f99f3150ecb0e936c67cca3d0b6ef7041e8
Mohamed Montasser <90785835+MohamedMontasser03@users.noreply.github.com>
2024-05-01 15:34:45 +0300

    Update led.c

--- led.c
+++ led.c
@@ -1,1 +1,16 @@
+#include "led.h"
 
+void RGB_set(char mask){
+    mask &= 0X0E;
+    GPIO_PORTF_DATA_R=mask;
+}
+
+void RGB_clear(char mask){
+    mask &= 0X0E;
+    GPIO_PORTF_DATA_R &= ~mask;
+}
+
+void RGB(char colour){
+	GPIO_PORTF_DATA_R &= ~RGB_LED;			// Stop all active LEDs
+	GPIO_PORTF_DATA_R |= colour;			// Turn on this led
+}
