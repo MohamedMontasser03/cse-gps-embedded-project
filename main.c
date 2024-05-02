b60054195e097e40cc4d41f1197a2d878629c5b4
yousef hatem <52169412+elganzory@users.noreply.github.com>
2024-05-02 21:40:46 +0200

    Update main.c
    add approximation and send_float function

--- main.c
+++ main.c
@@ -20,12 +20,8 @@ void portF_init(void);
 double to_degree(float raw_degree);
 double to_radians(double degrees);
 double distance(double lat1, double lon1, double lat2, double lon2);
-
-
-double to_degree(float raw_degree);
-double to_radians(double degrees);
-double distance(double lat1, double lon1, double lat2, double lon2);
-
+double approximate( double a, float d);
+void UART0SendFloat(float num);
 
 int main(void){
 	
@@ -103,4 +99,21 @@ double distance(double lat1, double lon1, double lat2, double lon2) {  //calc di
     double a = pow(sin(dlat / 2), 2) + cos(to_radians(lat1)) * cos(to_radians(lat2)) * pow(sin(dlon / 2), 2);
     double c = 2 * atan2(sqrt(a), sqrt(1 - a));
     return RADIUS * c;
+}
+
+
+double approximate( double a, float d) {
+	return (int)(a/d +0.5)*d;
+}
+
+
+void UART0SendFloat(float num) {
+    int i;
+    char buffer[50];
+    snprintf(buffer, sizeof(buffer), "%f", num);
+
+   for( i=0 ; buffer[i] != '\0'; i++) {
+        while((UART0_FR_R & UART_FR_TXFF)==UART_FR_TXFF); // Wait until the transmitter is not full
+        UART0_DR_R = buffer[i]; // Transmit the character
+    }
 }
