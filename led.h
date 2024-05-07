7be34ff58414228ed7d16b41f165a4d7cfd86909
Mohamed Montasser <mohamed.montasser0385@gmail.com>
2024-05-07 15:31:32 +0300

    improved formatting for led.h

--- led.h
+++ led.h
@@ -14,13 +14,12 @@
 #define RED_LED 0x02
 #define BLUE_LED 0x04
 #define GREEN_LED 0x08
-#define RGB_LED 0x0E 
-#define SW1       0x10 
-#define SW2       0x01 
+#define RGB_LED 0x0E
+#define SW1 0x10
+#define SW2 0x01
 
 void RGB_set(char mask);
 void RGB_clear(char mask);
 void RGB(char colour);
-
 
 #endif
