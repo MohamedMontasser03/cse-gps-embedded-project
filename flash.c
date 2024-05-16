80bb6c72b1deb0540554971be70871bbdf7e61f7
Demiana Ayman <133611525+Demi-ayman@users.noreply.github.com>
2024-05-16 22:17:57 +0300

    Update flash.c
    add Write function

--- flash.c
+++ flash.c
@@ -10,6 +10,40 @@ void Flash_Enable(void) {
         flashKey_ = 0x71D5;
     }
 }
+int Flash_Write(const void* data, int wordCount, uint32_t offset) {
+    int i;
+    // Make sure Enable was called.
+    if (flashKey_ == 0) {
+        return -1;
+    }
+
+    // Must erase the data first.  A write may only change a bit from 1 to 0, so
+    // if the
+    //	bit is already zero, the write fails.  Erasing will set all bits to 1's.
+    //  Calculate the number of 1KiB blocks that the data will span and erase
+    //  that many.
+
+    // Write one word at a time...
+    for (i = 0; i < wordCount; i++) {
+        // Set the data register.  This the word that will be written.
+        FLASH_FMD_R = ((volatile uint32_t*)data)[i];
+
+        // Clear then set the OFFSET address field (17:0) with the write
+        // address.
+        FLASH_FMA_R &= 0xFFFC0000;
+        FLASH_FMA_R |=
+            (uint32_t)FLASH_BASE_ADDR + ((i + offset) * sizeof(uint32_t));
+
+        // Trigger a write operation...
+        FLASH_FMC_R = (flashKey_ << 16) | 0x1;
+
+        // Poll the WRITE bit until it is cleared.
+        while (FLASH_FMC_R & 0x1) {
+        }
+    }
+
+    return 0;
+}
 
 void Flash_Read(void* data, int wordCount, uint32_t offset) {
     int i;
