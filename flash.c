a4278d6a439012cf9cd53c8853d136f4b73dcc9d
JanNabil73 <168550877+JanNabil73@users.noreply.github.com>
2024-05-16 22:03:08 +0300

    Update flash.c
    erase the flash

--- flash.c
+++ flash.c
@@ -18,3 +18,28 @@ void Flash_Read(void* data, int wordCount, uint32_t offset) {
         ((uint32_t*)data)[i] = FLASH_BASE_ADDR[i + offset];
     }
 }
+int Flash_Erase(int blockCount) {
+    // Make sure Enable was called.
+    int i;
+    if (flashKey_ == 0) {
+        return -1;
+    }
+
+    for (i = 0; i < blockCount; i++) {
+        // Clear then set the OFFSET (17:0) with the write address.
+        FLASH_FMA_R &= 0xFFFC0000;
+
+        // Blocks are erased on 1KiB boundaries, so multiply the index by 1024
+        //	and add this to the base address.
+        FLASH_FMA_R |= ((uint32_t)FLASH_BASE_ADDR) + (i * 1024);
+
+        // Set the ERASE command bit.
+        FLASH_FMC_R = (flashKey_ << 16) | 0x2;
+
+        // Poll the ERASE bit until it is cleared.
+        while (FLASH_FMC_R & 0x2) {
+        }
+    }
+
+    return 0;
+}
