0a402fb436fa628d6363733b422bf84b1811340f
Johnathan25 <168485693+Johnathan25@users.noreply.github.com>
2024-05-16 21:04:36 +0300

    Update lcd.c
    lcd initialization function

--- lcd.c
+++ lcd.c
@@ -23,4 +23,25 @@ void LCD_sendCommand(uint8_t command) {
     GPIO_PORTB_DATA_R &= 0xFB; /*enable =0*/
     delayMillis(1);
 }
+void LCD_init(void) {
+    SYSCTL_RCGCGPIO_R |= 0x02;                    /* activate Port B */
+    while ((SYSCTL_PRGPIO_R &= 0x00000002) == 0); /*waite to activate*/
+    GPIO_PORTB_AMSEL_R &= 0;                      /* Disable Analog on PORTB*/
+    GPIO_PORTB_PCTL_R &=
+        0x00000000; /* Clear PMCx bits for  PORTB to use it as GPIO pin */
+    GPIO_PORTB_DIR_R |= 0xFF;  /* Configure PORTB as output pin */
+    GPIO_PORTB_AFSEL_R &= 0x0; /* Disable alternative function on PORTB */
+    GPIO_PORTB_DEN_R |= 0xFF;  /* Enable Digital I/O onPORTB */
+    GPIO_PORTB_DATA_R &= 0x0;  /* Clear data in PORT B */
+
+    delayMillis(20);        // delay 20ms
+    LCD_sendCommand(0X33);  // wakeup
+    delayMillis(5);         // DELAY 5mS
+    LCD_sendCommand(0X32);  // wakeup
+    LCD_sendCommand(0X28);  /* Select 4-bit Mode of LCD */
+    LCD_sendCommand(0X06);  /* clear whatever is written on display */
+    LCD_sendCommand(0X01);  /* shift cursor right */
+    LCD_sendCommand(0X0F);  /* Enable Display and cursor blinking */
+    LCD_sendCommand(0x02);
+}
 
