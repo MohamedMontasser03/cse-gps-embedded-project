d4b47b89845c100d9666782cbd559182d69677c2
yousef hatem <52169412+elganzory@users.noreply.github.com>
2024-05-16 19:50:34 +0200

    moved distance functions to gps

--- gps.c
+++ gps.c
@@ -57,3 +57,26 @@ void GPS_format(void) {
             currentLong = -(atof(GPS_formated[4]));
     }
 }
+
+double to_degree(float raw_degree) {  // gps output to degrees
+    int dd = (int)(raw_degree / 100);
+    double mm = raw_degree - (dd * 100);
+    double degree = dd + (mm / 60);
+    return degree;
+}
+
+double to_radians(double degrees) {  // degree to rad
+    return degrees * M_PI / 180.0;
+}
+
+double distance(double lat1, double lon1, double lat2,
+                double lon2) {  // calc distance between 2 coordinates in M
+    double dlat = to_radians(lat2 - lat1);
+    double dlon = to_radians(lon2 - lon1);
+    double a = pow(sin(dlat / 2), 2) + cos(to_radians(lat1)) *
+                                           cos(to_radians(lat2)) *
+                                           pow(sin(dlon / 2), 2);
+    double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+    return RADIUS * c;
+}
+
