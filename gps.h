fff5b08f17341cdc5fa8e9cadd2feefb2eb34ebf
yousef hatem <52169412+elganzory@users.noreply.github.com>
2024-05-16 20:28:54 +0200

    added some constants

--- gps.h
+++ gps.h
@@ -1,5 +1,8 @@
 #ifndef GPS_H_INCLUDED
 #define GPS_H_INCLUDED
+#define M_PI 3.14159265358979323846
+#define RADIUS 6371000  // radius of earth in meters
+
 void GPS_read(void);
 void GPS_format(void);
 float to_degree(float raw_degree);
