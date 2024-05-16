ff9e286c43d24ed7c1fdeeb7f4c5ecdeb9f39250
yousef hatem <52169412+elganzory@users.noreply.github.com>
2024-05-16 19:51:18 +0200

    added distance functions in gps header

--- gps.h
+++ gps.h
@@ -2,5 +2,8 @@
 #define GPS_H_INCLUDED
 void GPS_read(void);
 void GPS_format(void);
+float to_degree(float raw_degree);
+double to_radians(double degrees);
+double distance(double lat1, double lon1, double lat2, double lon2);
 
 #endif
